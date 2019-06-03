282870ff52de2d030f807a44f78d01f950d1562f
bart369 <bartinpeguero@gmail.com>
2019-06-02 22:36:12 -0400

    Fixed some spacing issues

--- S01-L02-Solution.sql
+++ S01-L02-Solution.sql
@@ -1,32 +1,27 @@
 SELECT
-    `employee
-`.`id` AS employee_id, 
-    CONCAT
-(`employee`.`first_name`, ' ', `employee`.`last_name`) AS employee_full_name, 
+    `employee`.`id` AS employee_id, 
+    CONCAT(`employee`.`first_name`, ' ', `employee`.`last_name`) AS employee_full_name, 
     `department`.`id` AS department_id, `department`.`name` AS last_department_name
-FROM employee 
+FROM employee /* Do not shorten table name aliases SO Ileft this simply as employee */
 
-INNER JOIN
-( 
+INNER JOIN ( /* Subquery starts in the next line */
     SELECT
-    `department_employee_rel
-`.`employee_id`, max
-(`department_employee_rel`.`id`) AS max_id
+        `department_employee_rel`.`employee_id`, 
+        MAX(`department_employee_rel`.`id`) AS max_id
     FROM department_employee_rel 
-    WHERE 1=1
+    WHERE 1=1 /* Use 1=1  */
         AND `department_employee_rel`.`deleted_flag` = 0
     GROUP BY `department_employee_rel`.`employee_id` 
     ) AS department_employee_rel_id ON `department_employee_rel_id`.`employee_id` = `employee`.`id` 
-
+-- Space between Multiple Joins
 INNER JOIN department_employee_rel ON 1=1
-    AND `department_employee_rel`.`id` = department_employee_rel_id.max_id 
+    AND `department_employee_rel`.`id` = department_employee_rel_id.max_id  /* Abusing AND because it's awesome */
     AND `department_employee_rel`.`deleted_flag` = 0 
 
 INNER JOIN department ON 1=1 
     AND `department`.`id` = `department_employee_rel`.`department_id `
     AND `department`.`deleted_flag` = 0
 WHERE 1=1
-    AND `employee`.`id` IN
-(10010, 10040, 10050, 91050, 205357) 
+    AND `employee`.`id` IN(10010, 10040, 10050, 91050, 205357) 
     AND `employee`.`deleted_flag` = 0
 LIMIT 100; 
