4e6989cf52dcb90d1d112ced58d3e089ccc46750
bart369 <bartinpeguero@gmail.com>
2019-06-02 22:48:27 -0400

    Added some comments and fixed typo

--- S01-L02-Solution.sql
+++ S01-L02-Solution.sql
@@ -15,7 +15,7 @@ INNER JOIN ( /* Subquery starts in the next line */
     ) AS department_employee_rel_id ON `department_employee_rel_id`.`employee_id` = `employee`.`id` 
 -- Space between Multiple Joins
 INNER JOIN department_employee_rel ON 1=1
-    AND `department_employee_rel`.`id` = department_employee_rel_id.max_id  /* Abusing AND because it's awesome */
+    AND `department_employee_rel`.`id` = `department_employee_rel_id`.`max_id`  /* Abusing AND because it's awesome */
     AND `department_employee_rel`.`deleted_flag` = 0 
 
 INNER JOIN department ON 1=1 
