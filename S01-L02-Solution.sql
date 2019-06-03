acf10ee3684a43685522158d70eb5a8e91d0a0e1
bart369 <bartinpeguero@gmail.com>
2019-06-02 23:00:17 -0400

    Issues committing the right changes

--- S01-L02-Solution.sql
+++ S01-L02-Solution.sql
@@ -2,7 +2,7 @@ SELECT
     `employee`.`id` AS employee_id, 
     CONCAT(`employee`.`first_name`, ' ', `employee`.`last_name`) AS employee_full_name, 
     `department`.`id` AS department_id, 
-    `department`.`name` AS last_department_name
+    `department`.`name` AS last_department_name   
 FROM `sample_staff`.`employee` 
 /* Do not shorten table name aliases SO I left this simply as employee 
     Also sample_staff is the name of the database. */
@@ -15,8 +15,8 @@ INNER JOIN ( /* Subquery starts in the next line */
     WHERE 1=1 /* Use 1=1  */
         AND `department_employee_rel`.`deleted_flag` = 0
     GROUP BY `department_employee_rel`.`employee_id` 
-    ) 
-    AS department_employee_rel_max ON `department_employee_rel_max`.`employee_id` = `employee`.`id` 
+    ) AS department_employee_rel_max ON 1=1
+    AND `department_employee_rel_max`.`employee_id` = `employee`.`id` 
 -- Space between Multiple Joins
 INNER JOIN department_employee_rel ON 1=1
     AND `department_employee_rel`.`id` = `department_employee_rel_max`.`max_id`  /* Abusing AND because it's awesome */
