932d10954fa4c70ec4d6a45caf6487cfd4223b85
bart369 <bartinpeguero@gmail.com>
2019-06-02 22:55:24 -0400

    Renamed department_employee_rel_id to department_remployee_rel_max

--- S01-L02-Solution.sql
+++ S01-L02-Solution.sql
@@ -1,8 +1,11 @@
 SELECT
     `employee`.`id` AS employee_id, 
     CONCAT(`employee`.`first_name`, ' ', `employee`.`last_name`) AS employee_full_name, 
-    `department`.`id` AS department_id, `department`.`name` AS last_department_name
-FROM employee /* Do not shorten table name aliases SO Ileft this simply as employee */
+    `department`.`id` AS department_id, 
+    `department`.`name` AS last_department_name
+FROM `sample_staff`.`employee` 
+/* Do not shorten table name aliases SO I left this simply as employee 
+    Also sample_staff is the name of the database. */
 
 INNER JOIN ( /* Subquery starts in the next line */
     SELECT
@@ -12,14 +15,15 @@ INNER JOIN ( /* Subquery starts in the next line */
     WHERE 1=1 /* Use 1=1  */
         AND `department_employee_rel`.`deleted_flag` = 0
     GROUP BY `department_employee_rel`.`employee_id` 
-    ) AS department_employee_rel_id ON `department_employee_rel_id`.`employee_id` = `employee`.`id` 
+    ) 
+    AS department_employee_rel_max ON `department_employee_rel_max`.`employee_id` = `employee`.`id` 
 -- Space between Multiple Joins
 INNER JOIN department_employee_rel ON 1=1
-    AND `department_employee_rel`.`id` = `department_employee_rel_id`.`max_id`  /* Abusing AND because it's awesome */
+    AND `department_employee_rel`.`id` = `department_employee_rel_max`.`max_id`  /* Abusing AND because it's awesome */
     AND `department_employee_rel`.`deleted_flag` = 0 
 
 INNER JOIN department ON 1=1 
-    AND `department`.`id` = `department_employee_rel`.`department_id `
+    AND `department`.`id` = `department_employee_rel`.`department_id`
     AND `department`.`deleted_flag` = 0
 WHERE 1=1
     AND `employee`.`id` IN(10010, 10040, 10050, 91050, 205357) 
